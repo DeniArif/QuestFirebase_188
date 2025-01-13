29d2642699d97391387f7f6c07e2cff0ed5233db
DeniArif <deniarifwibowo82@gmail.com>
2025-01-13 14:58:44 +0700

    Membuat kode di PengelolaHalaman.kt

---
+++ app/src/main/java/com/example/myapplication/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import com.example.myapplication.view.HomeScreen
+import java.lang.reflect.Modifier
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    )  {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = {navController.popBackStack()},
+                onNavigate = {navController.navigate(DestinasiHome.route)}
+            )
+        }
+    }
+}
