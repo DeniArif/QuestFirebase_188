15f9785c509c4478bbcc13f10134cae3e5796199
DeniArif <deniarifwibowo82@gmail.com>
2025-01-13 14:30:54 +0700

    Menambahkan kode fun insertMhs dan kode get mhs

--- app/src/main/java/com/example/myapplication/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/NetworkMahasiswaRepository.kt
@@ -3,28 +3,61 @@ package com.example.myapplication.repository
 import com.example.myapplication.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
 
 class NetworkMahasiswaRepository (
     private val firestore: FirebaseFirestore
 ):MahasiswaRepository
 {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal memanbahkan data mahasiswa:${e.message}")
+        }
+    }
     override suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
         val mhsCollection = firestore.collection("mahasiswa")
             .orderBy("nim", Query.Direction.DESCENDING)
             .addSnapshotListener { value, error ->
+
                 if (value != null) {
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)!!
                     }
-                    trySend(mhsList)
+                    trySend(mhsList) // memberikan fungsi untuk mengiri, data ke flow
                 }
             }
         awaitClose {
             mhsCollection.remove()
+        }
+    }
+    override suspend fun getMhs(nim: String): Mahasiswa {
+        val mhsDocumented = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocumented.remove()
+        }
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e:Exception) {
+            throw Exception("Gagal menghapus data mahasiswa:")
         }
     }
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
@@ -40,6 +73,6 @@ class NetworkMahasiswaRepository (
     }
 
     override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
-        TODO("Not yet implemented")
+
     }
 }
